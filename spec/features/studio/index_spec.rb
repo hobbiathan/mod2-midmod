f3d5ccfa561c971ce6b65befe865a6519468aa80
Ruben Pasillas <hobbiathan@gmail.com>
2022-02-22 09:26:50 -0700

    add 'visit' to before each block

--- spec/features/studio/index_spec.rb
+++ spec/features/studio/index_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'Studio Index' do
     @movie_two = @studio.movies.create!(title: "Saving Private Ryan", creation_year: 1998, genre: "Drama")
     @movie_three = @studio.movies.create!(title: "The Sum of All Fears", creation_year: 2002, genre: "Drama")
 
-
+    visit "/studios"
   end
 
   it 'Displays all studio names and locations' do
