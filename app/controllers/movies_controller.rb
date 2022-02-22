f05f1c9724f104019eac66ae0ff53b5f2259a402
Ruben Pasillas <hobbiathan@gmail.com>
2022-02-22 12:10:42 -0700

    begin new work

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,4 +4,14 @@ class MoviesController < ApplicationController
     @actors = @movie.actors.by_age
     @average_age = @movie.actors.average(:age)
   end
+
+  def new
+    @movie = Movie.find(params[:id])
+  end
+
+  def create
+    movie = Movie.find(params[:id])
+
+    redirect_to "/movies/#{movie.id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get '/studios', to: 'studios#index'
 
   get '/movies/:id', to: 'movies#show'
+  get '/movies/:id/new', to: 'movies#new'
+
+  post '/movies/:id/new', to: 'movies#create'
 end
