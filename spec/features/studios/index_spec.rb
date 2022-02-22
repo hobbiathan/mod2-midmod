319f497f4ae08a566898f58e817a9fb94db407e2
Ruben Pasillas <hobbiathan@gmail.com>
2022-02-22 09:45:53 -0700

    fix typo

---
+++ spec/features/studios/index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'Studio Index' do
+  before(:each) do
+    @studio = Studio.create!(name: "Universal Pictures", location: "California")
+    @studio_two = Studio.create!(name: "Paramount Global", location: "New York")
+
+    @movie = @studio.movies.create!(title: "Jurassic Park", creation_year: 1993, genre: "Science Fiction")
+    @movie_two = @studio.movies.create!(title: "Saving Private Ryan", creation_year: 1998, genre: "Drama")
+    @movie_three = @studio_two.movies.create!(title: "The Sum of All Fears", creation_year: 2002, genre: "Drama")
+
+    visit "/studios"
+  end
+
+  it 'Displays all studio names and locations' do
+    expect(page).to have_content(@studio.name)
+    expect(page).to have_content(@studio.location)
+
+    expect(page).to have_content(@studio_two.name)
+    expect(page).to have_content(@studio_two.location)
+  end
+
+  it 'Displays titles of all movies belonging to respective studio' do
+    expect(page).to have_content(@movie.title)
+    expect(page).to have_content(@movie_two.title)
+    expect(page).to have_content(@movie_three.title)
+  end
+end
