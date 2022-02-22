5d119c5d264e1241c1efd4bc2528209c054145ac
Ruben Pasillas <hobbiathan@gmail.com>
2022-02-22 11:49:52 -0700

    create ::by_age for actor model, update movie view to display actors accordingly

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,8 @@
 class Actor < ApplicationRecord
   has_many :movie_actors
   has_many :movies, through: :movie_actors
+
+  def self.by_age
+    order(age: :asc)
+  end
 end
--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe Actor do
     it { should have_many :movie_actors }
     it { should have_many(:movies).through(:movie_actors) }
   end
+
+  describe 'class methods' do
+    it '::by_age' do
+      @actor = Actor.create!(name: "Nam Neill", age: 74)
+      @actor_two = Actor.create!(name: "Laura Dern", age: 55)
+      expect(Actor.by_age).to eq([@actor_two, @actor])
+    end
+  end
 end
